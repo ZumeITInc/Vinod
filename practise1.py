f5c30745bf98f277c138b6df25a97f317b487fce
ZumeITInc <63601118+ZumeITInc@users.noreply.github.com>
2020-04-28 22:08:58 +0530

    Add files via upload

---
+++ practise1.py
@@ -0,0 +1,109 @@
+              #STRING PROGRAMS
+#string Program using CONCATINATION
+a='Hello World'
+b='This is beautiful Now'
+print(a+' '+b)
+print('\n')
+a='1'
+b='2'
+print(a+b)
+print('\n')
+#==============================
+#==============================
+
+#string program using INDEXING
+a='Data Structures'
+print(a[3])
+print('\n')
+#==============================
+#==============================
+
+#string program using SLICING
+a='advance python'
+print(a[1:6])
+print('\n')#from 1 index to 6 index position
+print(a[:])
+print('\n')#total string
+print(a[1:])
+print('\n')#from 1 index position
+print(a[:-1])
+print('\n')#from begining to end-1
+print(a[0::1])
+print('\n')#step function with 1 step
+print(a[:-1:2])
+print('\n')#step function with 2 steps 
+print(a[::-1])
+print('\n')#reverse order
+#================================
+#================================
+# strings using Methods
+
+a='hello world'
+print(a.upper())
+print('\n')
+a='HELLO WORLD'
+print(a.lower())
+print('\n')
+a='this is string with single coatation'
+print(a.split())
+print('\n')
+print(a.split('i'))
+print('\n')
+#================================
+#================================
+
+# Formating With .format() method
+print('This is String{}' .format(' INSERTED'))
+print('\n')
+print('The {} {} {}' .format('beauty' ,'with' ,'brain'))
+print('\n')
+print('The {q} {w} {d}' .format(q='quick', w='white', d='dog'))
+print('\n')
+#==================================
+#==================================
+#f' ' formated String
+name='Alexander'
+Hobby='Wars'
+print(f'{name} is Fund Of {Hobby}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
