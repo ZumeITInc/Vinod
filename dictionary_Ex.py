73fc0a9119e70cc283ad9a9c7e4b760cfa53af0d
ZumeITInc <63601118+ZumeITInc@users.noreply.github.com>
2020-04-29 20:09:28 +0530

    Add files via upload

---
+++ dictionary_Ex.py
@@ -0,0 +1,44 @@
+                        #DICTIONARY
+dict={'key1':'value1','key2':'value2','key3':'value3'}
+print(dict)
+print('\n')
+print(dict['key1'])
+print('\n')
+#===============================
+d={'apple':250,'mangoes':100,'bananas' :40}
+print('the price of bananas is {}'.format(d['bananas']))
+print('\n')
+#===============================
+d={'k1':123,'k2':[1,2,3],'k3':{'inside_key':456}}
+print(d)
+print('\n')
+print(d['k1'])
+print('\n')
+print(d['k2'])
+print('\n')
+print(d['k2'][2])
+print('\n')
+print(d['k3'])
+print('\n')
+print(d['k3']['inside_key'])
+print('\n')
+#===============================
+d={'key1':['a','b','c']}
+print(d)
+print('\n')
+print(d['key1'][1].upper())
+print('\n')
+#===============================
+d={'k1':1,'k2':2}
+print(d)
+print('\n')
+d['k3']=3
+print(d)
+print('\n')
+print(d.items())
+print('\n')
+print(d.keys())
+print('\n')
+print(d.values())
+
+   
