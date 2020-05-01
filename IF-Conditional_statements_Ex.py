e2073de220309d9eda30e2d33a51e321d546f201
ZumeITInc <63601118+ZumeITInc@users.noreply.github.com>
2020-05-01 18:49:52 +0530

    Add files via upload

---
+++ IF-Conditional_statements_Ex.py
@@ -0,0 +1,40 @@
+                  #Looping Statements
+
+          #IF Statement
+
+#Syntax : if (some_condition):
+             #execute this block
+#==========================================
+name = 'Alex'
+if name=='Alex':
+    print('Hello Alex')
+#==========================================
+          #ELIF Statement
+    
+#Syntax : if (some_condition):
+             #execute this block
+          #elif (some_condition):
+              #execute this block
+#==========================================
+name = 'Alexander'
+if name=='corey':
+    print("Hello Corey")
+elif  name=='Alexander':
+    print('Hello Alexander')
+#===========================================
+           #Else Statement
+#syntax : if (some_contion):
+              #execute this block
+          #elif (some_condition):
+              #Execute this block
+          #else:
+              #execute this block
+#============================================
+name= 'Michel'
+if name== 'adam':
+    print('hello adam')
+elif name== 'josh':
+    print('Hello Josh')
+else:
+    print('Hello Michel')
+          
